a1d0edce80856daaf5b1d5648cd391c946de63c5
lhj <3282657310@qq.com>
2019-02-13 23:46:54 +0800

    有一尺修改

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
     <el-container>
-      <el-header>
+      <el-header class="homeHeader">
           <el-row>
             <el-col :span="4">
-                <img src="" alt="图片加载失败">
+                 <img src="@/assets/logo.png" alt="图片加载失败">
             </el-col>
-            <el-col :span="19"></el-col>
-            <el-col :span="1"></el-col>
+            <el-col :span="19" class="homeCenter">
+                <h2>电商管理平台</h2>
+            </el-col>
+            <el-col :span="1" class="logout">
+                <a href="">退出</a>
+            </el-col>
            </el-row>
       </el-header>
       <el-container>
@@ -23,5 +27,15 @@ export default {};
 </script>
 
 <style>
-
+.homeHeader{
+    background: #999c;
+}
+.homeCenter{
+    color: #fff;
+    line-height: 60px;
+    text-align: center;
+}
+.logout{
+    line-height: 60px;
+}
 </style>
